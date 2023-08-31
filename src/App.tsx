27df8001c1360b1738b13572a9d46296e526e7cf
b6132552 <asosid15@gmail.com>
2023-08-31 18:44:44 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="min-h-screen flex justify-center items-center">
     <h1 className="text-3xl font-bold text-blue-600">
-    Install & Setup Vite + React + Typescript + Tailwind CSS 3
+       555555555555555555555555555555555555555555555555555555555555555
     </h1>
   </div>
   )
